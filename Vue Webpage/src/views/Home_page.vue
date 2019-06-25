442ecad87cac5b1e8d2e939b65e875763e01e876
yan-yan0129 <1842410827@qq.com>
2019-06-25 11:50:03 +0800

    提交

---
+++ Vue Webpage/src/views/Home_page.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Home">
+    <Homeheader></Homeheader>
+    <HomeNav></HomeNav>
+    <HomeCont></HomeCont>
+  </div>
+</template>
+
+<script>
+import Homeheader from "../components/Homeheader";
+import HomeNav from "../components/HomeNav";
+import HomeCont from"../components/HomeCont";
+export default {
+  name: "Home",
+  components: {
+    Homeheader,
+    HomeNav,
+    HomeCont,
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+.Home {
+  width: 100%;
+  height: 1080px;
+}
+</style>
